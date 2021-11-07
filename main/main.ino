12b7b0e26814b563a1eca343aa0f2c082d778376
= <jaramirezj@eafit.edu.co>
2021-11-07 10:19:30 -0500

    Initial commit

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,37 @@
-/*Desarrollado por Julian Ramirez Jimenez
+/*
+ * 
+ * Proyect developed by Julian Andres Ramirez Jimenez
+ * Student of Systems Enginering
  * Version 1
+ * 
+ * File designed to sense and send the data to
+ * the local server
  */
-//Archivo de control para el proyecto IoT
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+const char ssid = "UNE_HFC_7BB0";
+const char password = "AAA0BF87";
 
 void setup() {
-  
-
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ Serial.println("");
+ Serial.println("WiFi connected");
+ Serial.println("IP address: ");
+ Serial.println(WiFi.localIP());
 }
 
 void loop() {
   
 
+}
+
+String sendValues(String data){
+  
 }
